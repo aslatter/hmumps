93e0f45a19957d658748e4c7534276eb5481860c
aslatter <aslatter@gmail.com>
2007-05-31 05:19:14 +0000

    MAray initial patch

---
+++ MArray.hs
@@ -0,0 +1,21 @@
+module MArray where
+
+-- Copyright 2007 Antoine Latter
+-- aslatter@gmail.com
+
+import Data.Map
+import MValue
+
+Data MArray = ValueCell MValue (Map MValue MArray)
+            | Empty (Map MValue MArray)
+
+
+-- Given an MArray an d a list of subscripts, maybe
+-- return the value associated with those subs.
+mIndex :: MArray -> [MValue] -> Maybe MValue
+mIndex (Empty _) []         = Nothing
+mIndex (ValueCell v _) []   = Maybe v
+mIndex (Empty map) (x:xs)   = do vc <- lookup x map
+                                 mIndex vc xs
+mIndex (ValueCell v map) (x:xs) = do vc <- lookup x map
+                                     mIndex vc xs
