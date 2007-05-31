153c520267bf7c5dea94c8576cb4459976ba4196
aslatter <aslatter@gmail.com>
2007-05-31 23:17:07 +0000

    Simplified MArray

--- MArray.hs
+++ MArray.hs
@@ -7,16 +7,12 @@ import Data.Map
 import MValue
 import Prelude hiding (lookup)
 
-data MArray = ValueCell MValue (Map MValue MArray)
-            | Empty (Map MValue MArray)
+type MArray = (Maybe MValue, Map MValue MArray)
 
 
 -- Given an MArray an d a list of subscripts, maybe
 -- return the value associated with those subs.
 mIndex :: MArray -> [MValue] -> Maybe MValue
-mIndex (Empty _) []         = Nothing
-mIndex (ValueCell v _) []   = Just v
-mIndex (Empty map) (x:xs)   = do vc <- lookup x map
-                                 mIndex vc xs
-mIndex (ValueCell v map) (x:xs) = do vc <- lookup x map
-                                     mIndex vc xs
+mIndex (v, _map) []     = v
+mIndex (v, map)  (x:xs) = do vc <- lookup x map
+                             mIndex vc xs
