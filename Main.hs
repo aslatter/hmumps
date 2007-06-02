9d2698e2100e3d8df61fc14c4fc3ff22c32b7ab3
aslatter <aslatter@gmail.com>
2007-06-02 23:19:38 +0000

    More work on the ASTs

--- Main.hs
+++ Main.hs
@@ -3,8 +3,7 @@ module Main where
 -- Copyright 2007 Antoine Latter
 -- aslatter@gmail.com
 
-import Text.ParserCombinators.Parsec
-
+-- import Text.ParserCombinators.Parsec
 import MValue
 
 
@@ -19,11 +18,9 @@ import MValue
 -- isomorphic to unparsed MUMPS
 --
 -- It's intended that the execution environment will execute these
--- structures - so that it's a step removed from parsing.
-
-
--- NOTES: I expect I'll be scraping everything between this
---  and "Data Vn ..."
+-- structures - so that it's a step removed from parsing and line
+-- reading, even if the base execution environment still has a
+-- concept of "line"
 
 
 -- these commandlabels are going, and are only here for reference (as
@@ -36,26 +33,44 @@ data CommandLabel = Break | Close | Do | Else | For | Goto | Halt | Hang
 
 -- I feel like this is going to turn into an explosion of type contructors
 data Command = Break (Maybe Condition)
-             | Do (Maybe Condition) Location
+             | Do (Maybe Condition) [(Maybe Condition,Location,[FunArg])]
              | Else
-             | Goto (Maybe Condition) Location
+             | Goto (Maybe Condition) [(Maybe Condition,Location)]
              | Halt (Maybe Condition)
              | Hang (Maybe Condition) Expression
              | If [Condition]
              | Kill (Maybe Condition) [KillArg]
+             | Merge (Maybe Condition) [MergeArg]
              | New (Maybe Condition) [NewArg]
              | Set (Maybe Condition) [SetArg]
 
 
+-- Location stuff used by Do, Goto &c. Section 8.1.6.1
+data Location = Routine Routineref
+              | Subroutine DLabel (Maybe Integer) Routineref
+
+data DLabel = DLabel String 
+            | DLabelIndirect String
+
+data Routineref = Routineref (Maybe String) String
+                | RoutinerefIndirect String
 
 
 type Condition = Expression
 
 
+-- Variable names: 7.1.2
 data Vn = Lvn String [Expression]
         | Gvn String [Expression]
         | IndirectVn String [Expression]
 
--- there's somehting I'm not groking wrt the standard and expressions
+
+data FunArg = FunArgExp Expression
+            | FunArgName Name
+
+data Name = Name String | NameIndirect String
+
+-- there's somehting I'm not groking wrt the standard and expressions.
+-- The "Exression' type will ikely be the last thing I define.
 type Expression = [ExprAtom]
 data ExprAtom = ExprVn Vn 
