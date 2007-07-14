d17fd4e883e5d9431cb99a9721d903a23007dea0
aslatter <aslatter@gmail.com>
2007-07-14 19:00:36 +0000

    Added LISCENSE info

--- Main.hs
+++ Main.hs
@@ -10,9 +10,9 @@ import Text.ParserCombinators.Parsec
 -- input.  I don't know why
 
 main :: IO ()
-main = loop $ do putStr "> "
-                 x <- getLine
-                 putStrLn $ repl x
+main = disclaimer >> (loop $ do putStr "> "
+                                x <- getLine
+                                putStrLn $ repl x)
 
 loop :: Monad m => m () -> m ()
 loop ma = ma >> (loop ma)
@@ -21,3 +21,11 @@ repl :: String -> String
 repl x = case parse command "" x of
          Left err -> show err
          Right expTree -> show expTree
+
+disclaimer :: IO ()
+disclaimer = do putStrLn "HMUMPS  Copyright (C) 2007  Antoine Latter, Creighton Hogg"
+                putStrLn "This program comes with ABSOLUTELY NO WARRANTY; for details see"
+                putStrLn "the enclosed LISCENSE file."
+                putStrLn "This is free software, and you are welcome to redistribute it"
+                putStrLn "under certain conditions; for details see the enclosed LISCENSE file."
+                putStrLn ""
